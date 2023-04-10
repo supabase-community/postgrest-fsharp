1ec59fabb334315ea6f3e55fd432d0376a271b0c
Samo8 <samodubovec@gmail.com>
2023-04-10 10:14:34 +0200

    test: fixed broken tests due to refactoring

--- src/PostgrestTests/CommonTests.fs
+++ src/PostgrestTests/CommonTests.fs
@@ -2,9 +2,7 @@ module CommonTests
 
 open FsUnit.Xunit
 open Postgrest
-open Postgrest.Connection
 open Xunit
-
 
 [<Collection("parseColumns")>]
 module ParseColumnsTests =
@@ -24,7 +22,7 @@ module ParseColumnsTests =
     let ``returns * if columns is Cols with empty list`` () =
         // Arrange
         let expectedResult = "*"
-        let columns = Cols []
+        let columns = Columns []
 
         // Act
         let result = Common.parseColumns columns 
@@ -36,70 +34,10 @@ module ParseColumnsTests =
     let ``returns given columns if columns is Cols with non empty list`` () =
         // Arrange
         let expectedResult = "col-1,col-2"
-        let columns = Cols [ "col-1" ; "col-2"]
+        let columns = Columns [ "col-1" ; "col-2"]
 
         // Act
         let result = Common.parseColumns columns 
-
-        // Assert
-        result |> should equal expectedResult
-        
-[<Collection("getUrlSuffixFromPostgresFilterBuilder")>]
-module GetUrlSuffixFromPostgresFilterBuilderTests =
-    [<Fact>]
-    let ``returns string with table name if none of PostgrestFilterBuilder is set`` () =
-        // Arrange
-        let connection = postgrestConnection {
-            url "https://xxx.supabase.co/rest/v1"
-            headers  (Map [ "apiKey", "exampleApiKey" ])
-        }
-    
-        let expectedResult = "table?select=*"
-        let pfb =
-            { Query = { Connection = connection ; Table = "table" ; QueryString = "?select=*" }
-              QueryFilterString = None
-              QueryInString     = None
-              QueryIsString     = None
-              QueryOrderString  = None
-              QueryLimitString  = None
-              QueryOffsetString = None
-              QueryLikeString   = None
-              QueryILikeString  = None
-              QueryFtsString    = None
-              Body              = None
-              RequestType       = Select }
-
-        // Act
-        let result = Common.getUrlSuffixFromPostgresFilterBuilder pfb 
-
-        // Assert
-        result |> should equal expectedResult
-        
-    [<Fact>]
-    let ``returns string with table name and all of given PostgrestFilterBuilder params`` () =
-        // Arrange
-        let connection = postgrestConnection {
-            url "https://xxx.supabase.co/rest/v1"
-            headers  (Map [ "apiKey", "exampleApiKey" ])
-        }
-         
-        let expectedResult = "table?select=*&filter=f&in=in&is=is&order=o&limit=l&offset=o&like=l&ilike=i&fts=f"
-        let pfb =
-            { Query = { Connection = connection ; Table = "table" ; QueryString = "?select=*" }
-              QueryFilterString = Some "&filter=f"
-              QueryInString     = Some "&in=in"
-              QueryIsString     = Some "&is=is"
-              QueryOrderString  = Some "&order=o"
-              QueryLimitString  = Some "&limit=l"
-              QueryOffsetString = Some "&offset=o"
-              QueryLikeString   = Some "&like=l"
-              QueryILikeString  = Some "&ilike=i"
-              QueryFtsString    = Some "&fts=f"
-              Body              = None
-              RequestType       = Select }
-
-        // Act
-        let result = Common.getUrlSuffixFromPostgresFilterBuilder pfb 
 
         // Assert
         result |> should equal expectedResult
--- src/PostgrestTests/PostgrestFilterBuilderHelperTests.fs
+++ src/PostgrestTests/PostgrestFilterBuilderHelperTests.fs
@@ -1,2 +1,67 @@
-module Postgrest.Tests.PostgrestFilterBuilderHelperTests
+module PostgrestFilterBuilderHelperTests
+
+open Postgrest
+open Postgrest.Connection
+open FsUnit.Xunit
+open Xunit
+
+[<Collection("getUrlSuffixFromPostgresFilterBuilder")>]
+module GetUrlSuffixFromPostgresFilterBuilderTests =
+    [<Fact>]
+    let ``returns string with table name if none of PostgrestFilterBuilder is set`` () =
+        // Arrange
+        let connection = postgrestConnection {
+            url "https://xxx.supabase.co/rest/v1"
+            headers  (Map [ "apiKey", "exampleApiKey" ])
+        }
+    
+        let expectedResult = "table?select=*"
+        let pfb =
+            { Query = { Connection = connection ; Table = "table" ; QueryString = "?select=*" }
+              QueryFilterString = None
+              QueryInString     = None
+              QueryIsString     = None
+              QueryOrderString  = None
+              QueryLimitString  = None
+              QueryOffsetString = None
+              QueryLikeString   = None
+              QueryILikeString  = None
+              QueryFtsString    = None
+              Body              = None
+              RequestType       = Select }
+
+        // Act
+        let result = PostgrestFilterBuilderHelper.getUrlSuffixFromPostgresFilterBuilder pfb 
+
+        // Assert
+        result |> should equal expectedResult
+        
+    [<Fact>]
+    let ``returns string with table name and all of given PostgrestFilterBuilder params`` () =
+        // Arrange
+        let connection = postgrestConnection {
+            url "https://xxx.supabase.co/rest/v1"
+            headers  (Map [ "apiKey", "exampleApiKey" ])
+        }
+         
+        let expectedResult = "table?select=*&filter=f&in=in&is=is&order=o&limit=l&offset=o&like=l&ilike=i&fts=f"
+        let pfb =
+            { Query = { Connection = connection ; Table = "table" ; QueryString = "?select=*" }
+              QueryFilterString = Some "&filter=f"
+              QueryInString     = Some "&in=in"
+              QueryIsString     = Some "&is=is"
+              QueryOrderString  = Some "&order=o"
+              QueryLimitString  = Some "&limit=l"
+              QueryOffsetString = Some "&offset=o"
+              QueryLikeString   = Some "&like=l"
+              QueryILikeString  = Some "&ilike=i"
+              QueryFtsString    = Some "&fts=f"
+              Body              = None
+              RequestType       = Select }
+
+        // Act
+        let result = PostgrestFilterBuilderHelper.getUrlSuffixFromPostgresFilterBuilder pfb 
+
+        // Assert
+        result |> should equal expectedResult
 
